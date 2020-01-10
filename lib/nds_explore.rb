5443251d8ac8144e861f8dd2e5576ea1987bf8ef
Diana Wendt <dianakw@gmail.com>
2020-01-10 19:45:13 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -16,10 +16,11 @@ end
 
 def print_first_directors_movie_titles
   i = 0
-  while i < directors_database[0][:movies].length do
-    puts directors_database[0][:movies][i][:title]
-    i++
-  end
+  #while i < directors_database[0][:movies].length do
+  #  puts directors_database[0][:movies][i][:title]
+   # i++
+  #end
+  puts directors_database[0][:movies][0][:title]
 end
 
 print_first_directors_movie_titles
